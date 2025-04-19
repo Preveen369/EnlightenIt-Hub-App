22c28ccc09aef1bc02d5e4da05bd5fbf15ba68df
Preveen369 <spreveen123@gmail.com>
2025-04-19 09:50:08 +0530

    Added initial setup with pages, components, images, route links

---
+++ client/src/components/Posts.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import Thumbnail1 from '../images/blog1.jpg';
+import Thumbnail2 from '../images/blog2.jpg';
+import Thumbnail3 from '../images/blog3.jpg';
+import Thumbnail4 from '../images/blog4.jpg';
+
+
+const Posts = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Posts
