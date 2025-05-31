52c2d25bc144a5c771086987f561b65cb5fdac69
Preveen369 <spreveen123@gmail.com>
2025-05-31 20:15:09 +0530

    Update CORS origin to allow requests from the production URL

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const { notFound, errorHandler } = require('./middleware/errorMiddleware');
 const app=express();
 app.use(express.json({extended:true}));
 app.use(express.urlencoded({extended:true}));
-app.use(cors({credentials:true, origin: "http://localhost:3000/"}));
+app.use(cors({credentials:true, origin: "https://enlightenit-hub-app.onrender.com"}));
 app.use(fileUpload({ useTempFiles: true })); // <-- add this middleware
 
 app.use('/api/users', userRoutes);
